d519f9e74443c9ce0b0c61f116d2086e0719ed08
William Aguirre <aguirrewilliam-1996@hotmail.com>
2018-11-10 16:25:30 -0500

    Update FSM_Semaforo.vhd

--- FSM_Semaforo/FSM_Semaforo.vhd
+++ FSM_Semaforo/FSM_Semaforo.vhd
@@ -4,52 +4,50 @@ use ieee.std_logic_unsigned.all;
 
 --DEFINICIÓN DE LA ENTIDAD
 Entity FSM_Semaforo is port(
-	carro 					: in std_logic;
 	clock 					: in std_logic;
 	Fin_tiempo 				: in std_logic;
-	activar_temporizador : in std_logic;
 	sensor 					: in std_logic;
-	verde_avenida 			: out std_logic
+	activar_temporizador : out std_logic;
+	verde_avenida 			: out std_logic;
+	verde_calle				: out std_logic
 );
 end entity;
 
 --DEFINICIÓN DE LA ARQUITECTURA
 Architecture FSM_Semaforo_arq of FSM_Semaforo is
 	type estados_semaf is (rojo, verde);
-	signal estadoActual : estados_semaf;
+	shared variable estadoActual : estados_semaf := verde;
+	shared variable estadoSiguiente : estados_semaf := verde;
 begin
-	FSM1: process (clock)
-	begin
-		if (rising_edge(clock)) then
-			if (carro = '1' and Fin_tiempo = '0') then
-				sensor <= '1';
-				verde_avenida <= '0';
-				activar_temporizador <= '1';
-				estadoActual <= rojo;
-			else
-				sensor <= '1';
-				verde_avenida <= '1';
-				activar_temporizador <= '0';
-				estadoActual <= verde;
-			end if;
+	process(clock, sensor)
+		begin
+		estadoActual := estadoSiguiente;
+	end process;
+
+	process (clock)
+		begin
+		if rising_edge(clock) then
+			case estadoActual is
+				when verde =>
+					if(sensor ='1') then
+						activar_temporizador <= '1';
+						estadoSiguiente := rojo;
+					end if;
+					activar_temporizador <='0';
+					verde_avenida <= '1';
+					verde_calle <= '0';
+				when rojo =>
+					if(sensor ='0' and fin_tiempo ='1') then
+						estadoSiguiente := verde;
+					end if;
+					activar_temporizador <='0';
+					verde_avenida <= '0';
+					verde_calle <= '1';
+			end case;
 		end if;
 	end process;
 
-	FSM2: process (clock)
-	begin
-	CASE estadoActual is
-		WHEN rojo =>
-			if () then
-			
-			end if;
-		WHEN verde =>
-			if () then
-			
-			end if;
-	
-	end process;
-
-	end Architecture;
+end Architecture;
 
 --GRUPO 04 
 --Jhoan Lozano
